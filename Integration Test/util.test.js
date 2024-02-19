182507f0fcac0d25641d184927f8f4b49e6b5aa4
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-02-19 19:40:17 +0530

    Update util.test.js

--- Integration Test/util.test.js
+++ Integration Test/util.test.js
@@ -1,11 +1,20 @@
-const { validateVoter } = require("./util.js");
+const {
+    describe,
+    it,
+    expect
+} = require("jest")
+const {
+    validateVoter
+} = require("./util.js");
 
-test("should output info about vote right", () => {
-  const info = validateVoter("John", 23);
-  expect(info).toBe("John is 23 years old and has vote right !");
-});
+describe("check valid voter", () => {
+    it("should output info about vote right", () => {
+        const info = validateVoter("John", 23);
+        expect(info).toBe("John is 23 years old and has vote right !");
+    });
 
-test("should output data-less case", () => {
-  const info = validateVoter();
-  expect(info).toBe("undefined is undefined years old and has vote right !");
-});
+    it("should output data-less case", () => {
+        const info = validateVoter();
+        expect(info).toBe("undefined is undefined years old and has vote right !");
+    });
+})
