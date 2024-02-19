f604d857e2231551017074229b5f36926af6ffd8
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-02-19 19:34:36 +0530

    Update util.test.js

--- Unit Test/util.test.js
+++ Unit Test/util.test.js
@@ -1,11 +1,20 @@
-const { generateInfo } = require("./util.js");
+const {
+    describe,
+    it,
+    expect
+} = require("jest")
+const {
+    generateInfo
+} = require("./util.js");
 
-test("should output name and age", () => {
-  const info = generateInfo("John", 23);
-  expect(info).toBe("John is 23 years old !");
-});
+describe("check valid name and age", () => {
+    it("should output name and age", () => {
+        const info = generateInfo("John", 23);
+        expect(info).toBe("John is 23 years old !");
+    });
 
-test("should output data-less case", () => {
-  const info = generateInfo();
-  expect(info).toBe("undefined is undefined years old !");
-});
+    it("should output data-less case", () => {
+        const info = generateInfo();
+        expect(info).toBe("undefined is undefined years old !");
+    });
+})
